a6b6849b69f8924815b9748f2c34211a2e406759
TheJhoxX <victorjorgesibaja@gmail.com>
2022-09-19 13:26:03 +0200

    PLANTEADO COMBINACIÓN CON INSERCIÓN DIRECTA
    Creo que ya se como hacer la ordenación directa

--- p1ADA/src/QuicksortModificado.java
+++ p1ADA/src/QuicksortModificado.java
@@ -30,6 +30,9 @@ public class QuicksortModificado {
         return lizquierda;
     }
 
+    /*
+    Algoritmo de Quicksort base que funciona
+     */
     public static void quicksort(ArrayList<Integer> a, int izq, int der) {
 
         int pivote=a.get(izq); // tomamos primer elemento como pivote
@@ -57,6 +60,47 @@ public class QuicksortModificado {
 
     }
 
+    public static void quicksortModificado(ArrayList<Integer> a, int izq, int der, int k) {
+
+        //En caso de que el subarray que se pasa como parámetro a la llamada sea de longitud <= 3
+        if ((der - izq + 1) <= k)
+        {
+            System.out.println("ENTRA A INSERCIÓN DIRECTA");
+            insercionDirecta();
+            
+
+        }
+        //Si el subarray que se inspecciona es de longitud > k
+        else
+        {
+            int pivote = a.get(izq); // tomamos primer elemento como pivote
+            int i = izq;         // i realiza la búsqueda de izquierda a derecha
+            int j = der;         // j realiza la búsqueda de derecha a izquierda
+            int aux;
+
+            while (i < j) {                          // mientras no se crucen las búsquedas
+                while (a.get(i) <= pivote && i < j) i++; // busca elemento mayor que pivote
+                while (a.get(j) > pivote) j--;           // busca elemento menor que pivote
+                if (i < j) {                        // si no se han cruzado
+                    aux = a.get(i);                      // los intercambia
+                    a.set(i, a.get(j));
+                    a.set(j, aux);
+                }
+            }
+
+            a.set(izq, a.get(j));      // se coloca el pivote en su lugar de forma que tendremos
+            a.set(j, pivote);      // los menores a su izquierda y los mayores a su derecha
+
+            if (izq < j - 1)
+                quicksort(a, izq, j - 1);          // ordenamos subarray izquierdo
+            if (j + 1 < der)
+                quicksort(a, j + 1, der);          // ordenamos subarray derecho
+
+        }
+
+
+    }
+
 
 
 
@@ -80,7 +124,20 @@ public class QuicksortModificado {
         return vector;
     }
 
-    // Implementación en Java del algoritmo Quicksort
+    public static void insercionDirecta(ArrayList<Integer> a){
+        int p, j;
+        int aux;
+        for (p = 1; p < a.size(); p++){ // desde el segundo elemento hasta
+            aux = a.get(p);           // el final, guardamos el elemento y
+            j = p - 1;            // empezamos a comprobar con el anterior
+            while ((j >= 0) && (aux < a[j])){ // mientras queden posiciones y el
+                // valor de aux sea menor que los
+                a.set(j+1, j);   // de la izquierda, se desplaza a
+                j--;               // la derecha
+            }
+            a.set(j+1, aux);       // colocamos aux en su sitio
+        }
+    }
     public static void main(String []args)
     {
         ArrayList<Integer> vector1;
